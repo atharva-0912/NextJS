8d187369f255a9b4058390a9b3ac8213559d0bb4
atharva-0912 <atharvabandhakar0@gmail.com>
2025-07-14 09:52:46 +0530

    add files

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimental_ppr = true;
+{
+    (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+        <SideNav />
+        </div>
+    </div>
+    );
+}
